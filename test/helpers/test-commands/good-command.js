b3a6150aef59a8d401100e547b7928ccb7da4126
Adam Boe <adamboe@outlook.com>
2021-02-10 16:11:25 -0800

    debug func tests

--- test/helpers/test-commands/good-command.js
+++ test/helpers/test-commands/good-command.js
@@ -3,6 +3,9 @@ if (args.length > 2) {
   args.shift()
   args.shift()
 }
+
+console.log('TEST args: ' + args)
+console.log("TEST args.join(' '): " + args.join(' '))
 for (const line of args.join(' ').split(/\r\n?|\n|\\r\\n?|\\n/)) {
   console.log(line)
 }
