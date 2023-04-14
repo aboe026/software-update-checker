74115c65dc774fd41ea4d1ae15830c0aacec0e04
Adam Boe <adamboe@outlook.com>
2023-04-13 20:24:31 -0700

    fix coverage view

--- scripts/update-dependencies.ts
+++ scripts/update-dependencies.ts
@@ -9,6 +9,7 @@ const DENY_LIST: string[] = [
   'inquirer', // waiting on https://github.com/SBoudrias/Inquirer.js/issues/1029
   'node-fetch', // v3 switches to ESM (not compatible with current CommonJS)
   '@types/node-fetch', // v3 switches to ESM (not compatible with current CommonJS)
+  'open', // v9 switches to ESM (not compatible with current CommonJS)
 ]
 ;(async () => {
   const outdated = await getOutdatedDependencies()
